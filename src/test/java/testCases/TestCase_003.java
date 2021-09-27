2c2481c165322222981d595fcc5e2be2a77cf5c1
Ashish0235 <58934006+Ashish0235@users.noreply.github.com>
2021-09-27 19:26:17 +0530

    test_cases_programs

---
+++ src/test/java/testCases/TestCase_003.java
@@ -0,0 +1,15 @@
+package testCases;
+
+public class TestCase_003 {
+	
+	public static void main(String[] args) {
+
+		int R=3,I=2; //two integer values
+
+		float P = (float)R/I; //Result of P saved as float value
+
+		System.out.println("Output is in float : "+P);
+		//Output is 1.5
+	}
+
+}
